4ca9cda2c643e4417ff8932f7bbe4b8060d44cfd
PuBgOp5109 <86190919+PuBgOp5109@users.noreply.github.com>
2021-06-22 13:44:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function iplPage(){
+    window.location = "iplPage.html";
+    console.log("seeing IPl teams and players");
+}
+function iccPage(){
+    window.location = "icc.html";
+    console.log("seeing ICC teams and players");
+}
+function rcbPage(){
+window.location = "rcb.html"
+}
+function dcPage(){
+    window.location = "dc.html"
+}
+function cskPage(){
+    window.location = "csk.html"
+}
+function pbksPage(){
+    window.location = "pbks.html"
+}
+function miPage(){
+    window.location = "mi.html"
+}
+function srhPage(){
+    window.location = "srh.html"
+}
+function kkrPage(){
+    window.location = "kkr.html"
+}
+function rrPage(){
+    window.location = "rr.html"
+}
